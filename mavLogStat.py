0c594274c6c1680d50b827cceae22572ab2ab6b4
troll <dima.pashko@gmail.com>
2021-11-28 01:36:42 -0500

    Flight statistics added

--- mavLogStat.py
+++ mavLogStat.py
@@ -27,6 +27,40 @@ def show_user_params(params):
     print(f'Stall prevention: {param_stall_prevention} - {"OK" if param_stall_prevention == 0.0 else "Warning!"}')
 
 
+def show_flight_stat(log):
+    max_alt = 0
+    last_bat_msg = None
+    last_gps_msg = None
+    first_gps_msg = None
+
+    while True:
+        msg = log.recv_match(type=['BAT', 'BARO', 'GPS'])
+        if msg is None:
+            break
+
+        mtype = msg.get_type()
+
+        if mtype == 'BAT':
+            last_bat_msg = msg
+
+        if mtype == 'GPS':
+            if first_gps_msg is None:
+                first_gps_msg = msg
+            last_gps_msg = msg
+
+        if mtype == 'BARO':
+            alt = msg.Alt
+            if alt > max_alt:
+                max_alt = alt
+
+    print('\nFlight statistics:')
+    print('Total time: %.2f sec' % (last_bat_msg.TimeUS/1000000))
+    print('Maximum altitude: %.2f m' % max_alt)
+    print('Used: %.2f mAh' % last_bat_msg.CurrTot)
+    print(f'Home location: {first_gps_msg.Lat}, {first_gps_msg.Lng}')
+    print(f'Last known location: {last_gps_msg.Lat}, {last_gps_msg.Lng}')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='mavLogStat')
     parser.add_argument('logfile', help='log file to analyze')
@@ -41,6 +75,8 @@ if __name__ == '__main__':
                                           progress_callback=progress_bar)
 
         mlog.flightmode_list()
-        print(f'\n Done with message number: {mlog._count}')
+        print(f'\nLoaded {mlog._count} messages total')
 
         show_user_params(mlog.params)
+
+        show_flight_stat(mlog)
